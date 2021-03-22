a1b6451b9196a6e8f4dd95855311d16aaac79f84
yanelle <yanelle1@hotmail.com>
2021-03-22 22:06:44 +0100

    routes gestructureerd

--- lab5/routes/api/v1/todos.js
+++ lab5/routes/api/v1/todos.js
@@ -1,55 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const apiTodosController= require('../../../controllers/api/v1/messages');
 
-router.get("/", (req, res) => {
-    res.json({
-        "status": "success",
-        "message":"POSTING a new message for user Pikachu"
-    })
-})
+router.get("/", apiTodosController.getAll);
 
-router.get("/api/v1/messages", (req, res) => {
-    res.json({
-        "status": "success",
-        "message":"GETTING messages"
-    })
-})
+router.get(":id", apiTodosController.getId);
 
-router.get("/api/v1/messages/:id", (req, res) => {
-    let id= req.params.id;
+router.post("/", apiTodosController.post); 
 
-    res.json({
-        "status": "success",
-        "message":`GETTING message with ID ${id}`
-    })
-})
+router.put(":id", apiTodosController.update);
 
-router.post("/api/v1/messages/", (req, res) => {
-    res.json({
-        "status": "success",
-        "message":"POSTING a new message for user Pickachu"
-    })
-})
+router.delete(":id", apiTodosController.remove);
 
-router.put("/api/v1/messages/:id", (req, res) => {
-    res.json({
-        "status": "success",
-        "message":`UPDATING a message with ID ${id}`
-    })
-})
-
-router.delete("/api/v1/messages/:id", (req, res) => {
-    res.json({
-        "status": "success",
-        "message":`DELETING a message with ID ${id}`
-    })
-})
-
-router.get("/api/v1/messages?user=username", (req, res) => {
-    let username= req.params.username;
-
-    res.json({
-        "status": "success",
-        "message":`GETTING a message for username ${username}`
-    })
-})
+module.exports= router;
