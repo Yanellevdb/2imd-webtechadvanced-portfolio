dd24c6845fb922cfbee2bc9aca7a8936f3f584bf
yanelle <yanelle1@hotmail.com>
2021-03-22 22:35:51 +0100

    getAll added

--- lab5/controllers/api/v1/messages.js
+++ lab5/controllers/api/v1/messages.js
@@ -1,37 +1,46 @@
-const apiTodosRouter= require('../../../routes/api/v1/todos');
-const express= require('express')
-const router= express.Router();
-
 const getId= (req, res) => {
     let id= req.params.id;
     res.json({
-        "status": "success",
-        "message":`GETTING message with ID ${id}`
+        status: "success",
+        message:`GETTING message with ID ${id}`
     });
-})
+};
 
 const post= (req, res) => {
     res.json({
-        "status": "success",
-        "message":"POSTING a new message for user Pickachu"
+        status: "success",
+        message:"POSTING a new message for user Pickachu"
     });
-})
+};
 
 const update= (req, res) => {
     let id= req.params.id;
     res.json({
-        "status": "success",
-        "message":`UPDATING a message with ID ${id}`
+        status: "success",
+        message:`UPDATING a message with ID ${id}`
     });
-})
+};
 
 const delete= (req, res) => {
     let id= req.params.id;
     res.json({
-        "status": "success",
-        "message":`DELETING a message with ID ${id}`
+        status: "success",
+        message:`DELETING a message with ID ${id}`
     });
-})
+};
 
-
-module.exports= router;
+const getAll= (req, res) => {
+    let user= req.query.user;
+    if(req.query.user){
+        res.json({
+            status: "success",
+            message:`GETTING messages from username ${user}`
+        })
+        };
+    else{
+    res.json({
+        status: "success",
+        message:"GETTING messages"
+    });
+    }
+}
