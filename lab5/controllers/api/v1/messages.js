7a7431cee80cf2d9b6d0f492b6290ddb795849d3
yanelle <yanelle1@hotmail.com>
2021-03-22 22:22:50 +0100

    vier routers toegevoegd aan messages

--- lab5/controllers/api/v1/messages.js
+++ lab5/controllers/api/v1/messages.js
@@ -2,57 +2,36 @@ const apiTodosRouter= require('../../../routes/api/v1/todos');
 const express= require('express')
 const router= express.Router();
 
-(req, res) => {
-    res.json({
-        "status": "success",
-        "message":"POSTING a new message for user Pikachu"
-    })
-})
-
-(req, res) => {
-    res.json({
-        "status": "success",
-        "message":"GETTING messages"
-    })
-})
-
-(req, res) => {
+const getId= (req, res) => {
     let id= req.params.id;
-
     res.json({
         "status": "success",
         "message":`GETTING message with ID ${id}`
-    })
+    });
 })
 
-(req, res) => {
+const post= (req, res) => {
     res.json({
         "status": "success",
         "message":"POSTING a new message for user Pickachu"
-    })
+    });
 })
 
-(req, res) => {
+const update= (req, res) => {
+    let id= req.params.id;
     res.json({
         "status": "success",
         "message":`UPDATING a message with ID ${id}`
-    })
+    });
 })
 
-(req, res) => {
+const delete= (req, res) => {
+    let id= req.params.id;
     res.json({
         "status": "success",
         "message":`DELETING a message with ID ${id}`
-    })
+    });
 })
 
-(req, res) => {
-    let username= req.params.username;
-
-    res.json({
-        "status": "success",
-        "message":`GETTING a message for username ${username}`
-    })
-})
 
 module.exports= router;
