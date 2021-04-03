55556e3ec33652424fef7274038f8bcd11c7ce0f
yanelle <yanelle1@hotmail.com>
2021-04-03 17:50:21 +0200

    fixed unnessesary code

--- lab4/app.js
+++ lab4/app.js
@@ -3,9 +3,6 @@ class App{
         this.getLocation();
         this.lat;
         this.lng;
-
-        this.apiKey= "ec3799858a6942a080d92923210304";
-       // this.getActivity();
     }
 
     getLocation(){
@@ -21,32 +18,27 @@ class App{
         this.getWeather();
     }
 
-    getWeather(){
-        //https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=metric
-    
+    getWeather(){    
        let url= `https://api.weatherapi.com/v1/forecast.json?key=ec3799858a6942a080d92923210304&q=${this.lat},${this.lng}`;
        fetch(url).then(response => {
-           //console.log(response);
            return response.json();
        }).then(data => {
-          // console.log(data);
-          // document.querySelector("#weather").innerHTML= data.current.temp_c;
            document.querySelector("#weather").innerHTML= "Today, it's " + data.current.temp_c + " degrees and " + data.current.condition.text;
 
             if (data.current.temp_c < 18){
-                    let url= `http://www.boredapi.com/api/activity?type=relaxation`;
+                let url= `http://www.boredapi.com/api/activity?type=relaxation`;
             
-                    fetch(url).then(response => {
-                        console.log(response);
-                        return response.json();
-                    }).then(data => {
-                       document.querySelector("#activity").innerHTML= "That's the perfect moment to... " + data.activity;
-                       document.querySelector("#ad").style.backgroundImage="url('koud.jpg')";
-                       console.log(data);
-                    }).catch(err => {
-                        console.log(err);
-                
+                fetch(url).then(response => {
+                    console.log(response);
+                    return response.json();
+                }).then(data => {
+                    document.querySelector("#activity").innerHTML= "That's the perfect moment to... " + data.activity;
+                    document.querySelector("#ad").style.backgroundImage="url('koud.jpg')";
+                    console.log(data);
+                }).catch(err => {
+                    console.log(err);
                 })
+
             } else {
                 let socialurl= `http://www.boredapi.com/api/activity?type=social`;
 
@@ -70,8 +62,6 @@ class App{
     errorLocation(err){
         console.log(err);
     }
-
-
 
 }
 
